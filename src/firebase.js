19ec4a204d460ff79bf898a16c449b64d3c98bc9
Harsh529-droid <78313761+Harsh529-droid@users.noreply.github.com>
2023-05-19 02:36:26 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,7 @@ import {getStorage} from 'firebase/storage'
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCvhaniQlmvidzFuK0JSkG50ta-CBnckEo",
-  authDomain: "chatab-a0764.firebaseapp.com",
-  projectId: "chatab-a0764",
-  storageBucket: "chatab-a0764.appspot.com",
-  messagingSenderId: "1089131262579",
-  appId: "1:1089131262579:web:1e26261690e6f9ee7db006",
-  measurementId: "G-SNF66GZNDD"
+  //your firebase config data
 };
 
 // Initialize Firebase
